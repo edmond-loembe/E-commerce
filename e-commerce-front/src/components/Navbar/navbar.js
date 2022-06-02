ce26379a0193a2e59cd0219b95b93d9a1dd417f1
Edmond Loembe <edmond@ayctor.com>
2022-06-02 15:29:35 +0200

    test

--- e-commerce-front/src/components/Navbar/navbar.js
+++ e-commerce-front/src/components/Navbar/navbar.js
@@ -19,12 +19,12 @@ export default function navbar() {
                         <Nav.Link className="navv" href="/nouveaute">Nouveautès</Nav.Link>
                         <NavDropdown className="navv" title="Produits" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/produits">Figurines</NavDropdown.Item>
-                            <NavDropdown.Item href="/produits">Textile</NavDropdown.Item>
+                            <NavDropdown.Item href="/produits">Textiles</NavDropdown.Item>
                             <NavDropdown.Item href="/produits">Posters</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="/produits">Goodies</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link className="navv" href="/univers">Univers</Nav.Link>                     
+                        <Nav.Link className="navv" href="/univers">Univers</Nav.Link>
                         <Nav.Link className="navv" href="/contact">Contacts</Nav.Link>
                         <Nav.Link className="imgg" href="/login"><img className="img-fluid" src={'compte.png'} /></Nav.Link>
                         <Nav.Link className="imgg" href="/panier"><img className="img-fluid" src={'pannier.png'} /></Nav.Link>
